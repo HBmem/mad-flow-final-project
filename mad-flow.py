80017d012bd5f2f65382be69da0866fa909b01c0
Dustin Cannon <dustin.cannon@gmail.com>
2025-11-22 10:45:32 -0800

    Refactor graph into its own file

--- mad-flow.py
+++ mad-flow.py
@@ -1,12 +1,21 @@
 import argparse
-from ford_fulkerson import Graph
+from graph import Graph
+from ford_fulkerson import ford_fulkerson
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-g", "--graph", type=str, required=True, help="Path to the graph file")
-    parser.add_argument("-s", "--source", type=str, default='s', help="Source node (default: 's')")
-    parser.add_argument("-t", "--sink", type=str, default='t', help="Sink node (default: 't')")
+    parser.add_argument(
+        "-g", "--graph", type=str, required=True, help="Path to the graph file"
+    )
+    parser.add_argument(
+        "-s", "--source", type=str, default="s", help="Source node (default: 's')"
+    )
+    parser.add_argument(
+        "-t", "--sink", type=str, default="t", help="Sink node (default: 't')"
+    )
     args = parser.parse_args()
 
     graph = Graph(args.graph)
-    print("The maximum possible flow is:", graph.ford_fulkerson(args.source, args.sink))
+    print(
+        "The maximum possible flow is:", ford_fulkerson(graph, args.source, args.sink)
+    )
