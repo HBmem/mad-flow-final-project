51ad1479c90c550a480b94f4e611f41f011c911e
Dustin Cannon <dustin.cannon@bigcommerce.com>
2025-11-24 22:30:17 -0800

    Fix graph generators

--- graphGenerationCode/Random/BuildGraph.java
+++ graphGenerationCode/Random/BuildGraph.java
@@ -1,90 +1,130 @@
-    private void BuildGraph(String fileName, String directory, int vertices, int dense, int maxCapacity, int minCapacity)
-    {
-     Random random = new Random();
-     try{
-         String dirName = directory;//           
-           if (dirName.equals ("")) {dirName = ".";}
+import java.io.*;
+import java.util.*;
 
-	       File outputfile = new File(dirName, fileName);
-                  int[][] Graph = new int[vertices][vertices];
-	              int n, m;
-	              
-                         for ( n = 0; n < vertices; n++)
-                          for ( m = n+1; m < vertices; m++)
-                          {
-                              int randomInt = (random.nextInt((maxCapacity-minCapacity+1))+minCapacity) ;
+public class BuildGraph
+{
+	public static void main(String[] args) throws Exception
+	{
+		String fileName, directory;
+		int vertices, dense, maxCapacity, minCapacity;
 
-                              int k =(int) (1000.0*Math.random()/10.0);
-                              int b = (k < dense) ? 1 : 0;
-                              if(b == 0)
-                              {
-                                  Graph[n][m] = Graph[m][n] = b;
-                              }
-                              else
-                              {
-                                Graph[n][m] = Graph[m][n] = randomInt;
-                              }
-                            }
-               			  
-                      
-            PrintWriter output = new  PrintWriter(new FileWriter(outputfile));
+		System.out.println("\n\n---------------------------------------------------");
+		System.out.print("Enter directory (or press Enter for current): \t");
+		directory = GetString();
+		System.out.print("Enter number of vertices: \t\t\t");
+		vertices = GetInt();
+		System.out.print("Enter density (0-100): \t\t\t\t");
+		dense = GetInt();
+		System.out.print("Enter minimum capacity: \t\t\t");
+		minCapacity = GetInt();
+		System.out.print("Enter maximum capacity: \t\t\t");
+		maxCapacity = GetInt();
+		System.out.print("Enter output file name (without .txt): \t");
+		fileName = GetString() + ".txt";
+		System.out.println("---------------------------------------------------\n");
 
-            for(int x = 0; x < Graph.length; x++)
-            {
-                if(x == 0)
-                {
-                    for(int y = 0; y < Graph[x].length; y++)
-                    {
-                        String value = String.valueOf(Graph[x][y]);
-                        if(y != 0)
-                        {
-                            if(value.equals("0") == false)
-                            {
-                                output.print("s " + String.valueOf(y) + " " +  value +"\n");
-                            }
-                        }
-                    }
-                }
-                else
-                {
-                    if(x == Graph.length-1)
-                    {
-                        for(int y = 0; y < Graph[x].length; y++)
-                        {
-                            String value = String.valueOf(Graph[x][y]);
-                            if(y != 0)
-                            {
-                                if(value.equals("0") == false)
-                                {
-                                    output.print(String.valueOf(y) + " t " +  value +"\n");
-                                }
-                            }
-                        }
-                    }
-                    else
-                    {
-                        for(int y = 0; y < Graph[x].length; y++)
-                        {
-                            String value = String.valueOf(Graph[x][y]);
-                            if(y != 0)
-                            {
-                                if(value.equals("0") == false)
-                                {
-                                    output.print(x + " " + String.valueOf(y) + " " +  value +"\n");
-                                }
-                            }
-                        }
-                    }
-                }
-                
-            }
+		BuildGraph(fileName, directory, vertices, dense, maxCapacity, minCapacity);
+	}
 
-         output.close();
-       } 
-        catch (IOException e)
-          {
-            System.err.println("Error opening file" +e);
-            return;
-          }
-     System.out.print("\nDone");
- }
+	private static void BuildGraph(String fileName, String directory, int vertices, int dense, int maxCapacity, int minCapacity)
+	{
+		Random random = new Random();
+		try{
+			String dirName = directory;
+			if (dirName.equals("")) {dirName = ".";}
+
+			File outputfile = new File(dirName, fileName);
+			int[][] Graph = new int[vertices][vertices];
+			int n, m;
+
+			for (n = 0; n < vertices; n++)
+				for (m = n+1; m < vertices; m++)
+				{
+					int randomInt = (random.nextInt((maxCapacity-minCapacity+1))+minCapacity);
+
+					int k = (int) (1000.0*Math.random()/10.0);
+					int b = (k < dense) ? 1 : 0;
+					if(b == 0)
+					{
+						Graph[n][m] = Graph[m][n] = b;
+					}
+					else
+					{
+						Graph[n][m] = Graph[m][n] = randomInt;
+					}
+				}
+
+			PrintWriter output = new PrintWriter(new FileWriter(outputfile));
+
+			for(int x = 0; x < Graph.length; x++)
+			{
+				if(x == 0)
+				{
+					for(int y = 0; y < Graph[x].length; y++)
+					{
+						String value = String.valueOf(Graph[x][y]);
+						if(y != 0)
+						{
+							if(value.equals("0") == false)
+							{
+								output.print("s " + String.valueOf(y) + " " + value + "\n");
+							}
+						}
+					}
+				}
+				else
+				{
+					if(x == Graph.length-1)
+					{
+						for(int y = 0; y < Graph[x].length; y++)
+						{
+							String value = String.valueOf(Graph[x][y]);
+							if(y != 0)
+							{
+								if(value.equals("0") == false)
+								{
+									output.print(String.valueOf(y) + " t " + value + "\n");
+								}
+							}
+						}
+					}
+					else
+					{
+						for(int y = 0; y < Graph[x].length; y++)
+						{
+							String value = String.valueOf(Graph[x][y]);
+							if(y != 0)
+							{
+								if(value.equals("0") == false)
+								{
+									output.print(x + " " + String.valueOf(y) + " " + value + "\n");
+								}
+							}
+						}
+					}
+				}
+			}
+
+			output.close();
+		}
+		catch (IOException e)
+		{
+			System.err.println("Error opening file: " + e);
+			return;
+		}
+		System.out.print("\nDone\n");
+	}
+
+	// Helper functions
+	public static String GetString() throws IOException
+	{
+		BufferedReader stringIn = new BufferedReader(new InputStreamReader(System.in));
+		return stringIn.readLine();
+	}
+
+	public static int GetInt() throws IOException
+	{
+		String aux = GetString();
+		return Integer.parseInt(aux);
+	}
+}
